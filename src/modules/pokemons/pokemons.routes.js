5bc9f6f4a682ebde27493102cfd4e434cc0b524b
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2024-08-27 07:38:17 +0900

    Create pokemons.routes.js

---
+++ src/modules/pokemons/pokemons.routes.js
@@ -0,0 +1,15 @@
+import angular from 'angular';
+
+routes.$inject = ['$stateProvider'];
+export default function routes($stateProvider) {
+    $stateProvider
+    .state('pokemons', {
+        url: '/',
+        template: require('./pokemons.html'),
+        resolve: {
+            pokemons: ['PokemonsService', (PokemonsService) => {
+                return PokemonsService.getPokemons()
+            }]
+        }
+    })
+};
