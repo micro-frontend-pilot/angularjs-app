9ef8b7f11b55388e847ddf2d6498c5e99dee4afd
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2024-08-27 07:37:02 +0900

    Create index.js

---
+++ src/modules/index.js
@@ -0,0 +1,18 @@
+import angular from 'angular'
+import uirouter from 'angular-ui-router'
+
+import proute from './pokemons/pokemons.routes.js'
+import pokemonList from './pokemons/pokemon-list/pokemon-list.component'
+import PokemonsService from './pokemons/pokemons.service'
+
+import groute from './modules.general.routers.js';
+
+export const grmod = angular.module('ngapp.generalRouter', [uirouter])
+    .config(groute)
+    .name
+
+export const pokemon = angular.module('ngapp.pokemons', [uirouter])
+    .config(proute)
+    .component('pokemonList', pokemonList)
+    .service('PokemonsService', PokemonsService)
+    .name
