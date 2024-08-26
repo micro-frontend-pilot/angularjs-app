4e45ea354e6fa03d8ec93f9b50be35422be0505e
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2024-08-27 08:03:30 +0900

    Update modules.general.routers.js

--- src/modules/modules.general.routers.js
+++ src/modules/modules.general.routers.js
@@ -1,6 +1,14 @@
 routes.$inject = ['$stateProvider'];
 export default function routes($stateProvider) {
     $stateProvider
+    .state('home', {
+        url: '/',
+        template: `<div>home</div>`,
+    })
+    .state('notfound', {
+        url: '/notfound',
+        template: `<div>Not Found</div>`,
+    })
     .state('mfe', {
         url: '/mfe',
         template: require('./mfe/mfe.html'),
