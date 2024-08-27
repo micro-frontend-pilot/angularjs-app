552fb32c246bf751060e2f668e58b7f51f462368
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2024-08-27 14:06:57 +0900

    Create MfeExposeComponent.js

---
+++ src/modules/mfe-expose/MfeExposeComponent.js
@@ -0,0 +1,5 @@
+import { angular2react } from 'angular2react';
+import { mfeExpose, $injector } from './mfe-expose';
+
+const MfeExposeComponent = angular2react('mfeExpose', mfeExpose, $injector);
+export default MfeExposeComponent;
