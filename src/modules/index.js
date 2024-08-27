0999b454d3305651a86512037f522759108b5e32
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2024-08-27 14:07:23 +0900

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -6,6 +6,8 @@ import pokemonList from './pokemons/pokemon-list/pokemon-list.component'
 import PokemonsService from './pokemons/pokemons.service'
 
 import groute from './modules.general.routers.js';
+import mfeExposeRoute from './mfe-expose/mfe-expose.router.js';
+import mfeExposeComponent from './mfe-expose/mfe-expose.js';
 
 export const grmod = angular.module('ngapp.generalRouter', [uirouter])
     .config(groute)
@@ -16,3 +18,9 @@ export const pokemon = angular.module('ngapp.pokemons', [uirouter])
     .component('pokemonList', pokemonList)
     .service('PokemonsService', PokemonsService)
     .name
+
+export const mfeExpose = angular.module('ngapp.mfeExpose', [uirouter])
+    .config(mfeExposeRoute)
+    .component('mfeExpose', mfeExposeComponent)
+    .name
+
